cbb7f637cdb2d2aded94861bc53b05b3cf772eaa
mariegrace31 <mariegracebmg@gmail.com>
2023-08-07 18:32:09 +0200

    Update the nav links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Logo from './components/Logo';
 import Navbar from './components/Navbar';
+import {Routes, Route} from 'react-router-dom';
+
+const Rocket = () => <div>Rockets content</div>;
+const Mission = () => <div>Missions content</div>;
+const Profile = () => <div>Profile content</div>;
 
 const App = () => (
+  <Routes>
+    <Route path='/' element={}
+  </Routes>
   <div className="app">
     <Logo />
     <Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,9 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <nav className="navbar">
     <ul>
-      <li>
-        <NavLink to="/" exact activeClassName="active"> Rockets</NavLink>
-      </li>
-      <li>
-        <NavLink to="/about" activeClassName="active">Missions</NavLink>
-      </li>
-      <li>
-        <NavLink to="/contact" activeClassName="active">My Profile</NavLink>
-      </li>
+      <li><NavLink to="/"  activeClassName="active"> Rockets</NavLink></li>
+      <li><NavLink to="/about" activeClassName="active">Missions</NavLink></li>
+      <li><NavLink to="/contact" activeClassName="active">My Profile</NavLink></li>
     </ul>
   </nav>
 );
