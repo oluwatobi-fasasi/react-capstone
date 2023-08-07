4e7f9ee0ae23bca34b67a45d6cdd033fe6967ace
Sonika Kumar <sonikak004@gmail.com>
2023-08-07 15:56:10 +0100

    Add Logo.js and nav.js in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-function App() {
-  return (
-    <div>Hello</div>
-  );
-}
+import React from 'react';
+import Logo from './components/Logo';
+import Navbar from './components/Navbar';
+
+const App = () => (
+  <div className="app">
+    <Logo />
+    <Navbar />
+  </div>
+);
 
 export default App;
