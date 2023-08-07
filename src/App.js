a8517d8a70840d4a6443b0909ba51b2192bdef97
mariegrace31 <mariegracebmg@gmail.com>
2023-08-07 18:52:01 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Logo from './components/Logo';
 import Navbar from './components/Navbar';
-import {Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const Rocket = () => <div>Rockets content</div>;
 const Mission = () => <div>Missions content</div>;
 const Profile = () => <div>Profile content</div>;
 
 const App = () => (
+  <BrowserRouter>
+  <Logo />
+  <Navbar />
   <Routes>
-    <Route path='/' element={}
+    <Route path="/" element={Rocket}>
+      <Route index element={<Rockets />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/contact" element={<Missions />} />
+    </Route>
   </Routes>
-  <div className="app">
-    <Logo />
-    <Navbar />
-  </div>
+</BrowserRouter>
 );
 
 export default App;
