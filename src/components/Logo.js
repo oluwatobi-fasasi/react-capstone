aa54fe83be62dd9808f046fb148ffa6386d1b298
Sonika Kumar <sonikak004@gmail.com>
2023-08-07 15:56:25 +0100

    Add logo.js with logo and border

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => (
+  <div className="head-wrap">
+    <div className="logo">
+      <img src="./planet.png" alt="Logo" />
+      <h1>Space Travelers Hub</h1>
+    </div>
+  </div>
+);
+
+export default Logo;
