57a5a9e3330c61c3c56253d88414f5db2208f2e8
Sonika Kumar <sonikak004@gmail.com>
2023-08-08 17:28:37 +0100

    Add html layout for mission page

--- src/components/mission.js
+++ src/components/mission.js
@@ -2,3 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { reserveMission } from '../redux/mission/missionslice';
+  return (
+    <div className="mission-container">
+      <div className="mission-name">
+        <h2>{mission.mission_name}</h2>
+      </div>
+      <div className="mission-info">
+        <p className="miss-desc">{mission.description}</p>
+      </div>
+      <div className="mission-buttons">
+        <button type="submit" className="join-button" onClick={handleJoinMission}>
+          Join Mission
+        </button>
+      </div>
+    </div>
+  );
+};
