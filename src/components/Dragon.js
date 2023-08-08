b58c46173e9264654840880976a449483df81fb0
mariegrace31 <mariegracebmg@gmail.com>
2023-08-08 16:17:36 +0200

    Display dragons

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -10,6 +10,19 @@ export default function Dragon() {
   const dragons = useSelector(selectAll);
   console.log(dragons);
   return (
-    <div>This is DRAGON component</div>
+    <div>
+      <h2>Dragons Page </h2>
+      <ul>
+        {dragons.map(dragon => (
+          <li key={dragon.id}>
+            <h3>{dragon.name}</h3>
+            <p>{dragon.description}</p>
+            {dragon.flickr_images.length > 0 && (
+              <img src={dragon.flickr_images[0]} alt={`${dragon.name} Dragon`} />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
