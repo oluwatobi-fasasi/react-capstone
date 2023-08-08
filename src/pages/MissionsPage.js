2f4d67aa38d993f6fad384eb710222487570b05e
Sonika Kumar <sonikak004@gmail.com>
2023-08-08 17:31:40 +0100

    Fetch Data from the spaceX api

--- src/pages/MissionsPage.js
+++ src/pages/MissionsPage.js
@@ -3,8 +3,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setMissions } from '../redux/mission/missionslice';
 import Mission from '../components/mission';
 
-const Mission = () => (
-  <div>Missions page</div>
+  useEffect(() => {
+    const fetchMissions = async () => {
+      try {
+        const response = await fetch('https://api.spacexdata.com/v3/missions');
+        const data = await response.json();
+        dispatch(setMissions(data));
+      } catch (error) {
+        console.error('Error fetching missions:', error);
+      }
+    };
 );
 
 export default Mission;
