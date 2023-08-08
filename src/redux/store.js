a42ce9a5d783dd2d9caa815e3e93e9622b026b57
Sonika Kumar <sonikak004@gmail.com>
2023-08-08 17:33:22 +0100

    Import missionreducer from missionslice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dragonsSlice from './dragons/dragonsSlice';
+import missionReducer from './mission/missionslice';
 
 const store = configureStore({
   reducer: {
