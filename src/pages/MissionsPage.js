31b55a9af202a4a5423d934763cfe01f646d05ce
Sonika Kumar <sonikak004@gmail.com>
2023-08-08 17:31:13 +0100

    Import redux slice and components to mission page

--- src/pages/MissionsPage.js
+++ src/pages/MissionsPage.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMissions } from '../redux/mission/missionslice';
+import Mission from '../components/mission';
 
 const Mission = () => (
   <div>Missions page</div>
