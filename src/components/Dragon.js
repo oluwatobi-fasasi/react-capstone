8c4075963756ecd89aad68e0482a35db58d96f67
mariegrace31 <mariegracebmg@gmail.com>
2023-08-08 18:54:49 +0200

    Style the dragons

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -13,12 +13,16 @@ export default function Dragon() {
       <ul>
         {dragons.map((dragon) => (
           <li key={dragon.id}>
-            <h3>{dragon.name}</h3>
-            <p>{dragon.desc}</p>
-            <p><button type="submit">Reserve Dragon</button></p>
             {dragon.flickr_images.length > 0 && (
               <img className="dragon-image" src={dragon.flickr_images} alt={`${dragon.name} Dragon`} />
             )}
+            <div className="desc">
+              <h3 className="dragon-name">{dragon.name}</h3>
+              <p className="dragon-desc">{dragon.desc}</p>
+              <p>
+                <button className="dragon-btn" type="submit">Reserve Dragon</button>
+              </p>
+            </div>
           </li>
         ))}
       </ul>
