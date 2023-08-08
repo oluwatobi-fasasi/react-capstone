dfbd02370c7dc36525c460ab6c836ad644ad1708
Sonika Kumar <sonikak004@gmail.com>
2023-08-08 17:35:18 +0100

    Create mission slice for reserve mission, and cancel mission

--- src/redux/mission/missionslice.js
+++ src/redux/mission/missionslice.js
@@ -3,3 +3,29 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   missions: [],
 };
+const missionSlice = createSlice({
+  name: 'mission',
+  initialState,
+  reducers: {
+    setMissions: (state, action) => ({
+      ...state,
+      missions: action.payload,
+    }),
+    reserveMission: (state, action) => {
+      const missionId = action.payload;
+      return {
+        ...state,
+        missions: state.missions.map((mission) => (mission.mission_id === missionId
+          ? { ...mission, reserved: true } : mission)),
+      };
+    },
+    cancelMission: (state, action) => {
+      const missionId = action.payload;
+      return {
+        ...state,
+        missions: state.missions.map((mission) => (mission.mission_id === missionId
+          ? { ...mission, reserved: false } : mission)),
+      };
+    },
+  },
+});
