728f552c424da7f81127172d0f4b50b88cee915c
mariegrace31 <mariegracebmg@gmail.com>
2023-08-10 00:49:14 +0200

    display the missions on the profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div>Profile page</div>
-);
+const ProfilePage = () => {
+  const missions = useSelector((state) => state.mission.missions);
 
-export default Profile;
+  // Filter the joined missions (reserved missions)
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+
+  return (
+    <div className="profile-container">
+      <h2 className="mission-title">My Missions</h2>
+      <ul className="mission-table">
+        {joinedMissions.map((mission) => (
+          <li key={mission.mission_id} className="mission-row">
+            {mission.mission_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProfilePage;
