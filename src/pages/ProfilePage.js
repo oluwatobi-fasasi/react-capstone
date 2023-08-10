52e36a81635622a08da10eac8211624490aa4cde
Marie Grâce Bahati <mariegracebmg@gmail.com>
2023-08-10 17:51:13 +0200

    Update ProfilePage.js

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -9,21 +9,24 @@ const ProfilePage = () => {
   const joinedMissions = missions.filter((mission) => mission.reserved);
 
   return (
-    <div className="profile-container">
-      <h3 className="mission-title">My Missions</h3>
-      <ul className="mission-table">
-        {joinedMissions.map((mission) => (
-          <li key={mission.mission_id} className="mission-row">
-            {mission.mission_name}
-          </li>
-        ))}
-      </ul>
-    </div>
-          
     <div>
-      <Profile />
+      <div className="profile-container">
+        <h3 className="mission-title">My Missions</h3>
+        <ul className="mission-table">
+          {joinedMissions.map((mission) => (
+            <li key={mission.mission_id} className="mission-row">
+              {mission.mission_name}
+            </li>
+          ))}
+        </ul>
+      </div>
+            
+      <div>
+        <Profile />
+      </div>
     </div>
   );
 };
 
 export default ProfilePage;
+
