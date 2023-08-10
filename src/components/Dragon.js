2f2473766cfc8d465b3082c7cecddcda51981983
Sonika Kumar <sonikak004@gmail.com>
2023-08-10 05:38:17 +0100

    Add reserved badge span below dragon name

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -1,39 +1,63 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchDragonsAsync, selectAll, reserveDragon } from '../redux/dragons/dragonsSlice';
+import {
+  fetchDragonsAsync,
+  cancelDragonReservation,
+  reserveDragon,
+  selectAllDragons,
+} from '../redux/dragons/dragonsSlice';
 
 export default function Dragon() {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchDragonsAsync());
   }, [dispatch]);
-  const dragons = useSelector(selectAll);
+
+  const dragons = useSelector(selectAllDragons);
+
   const reserveHandler = (id) => {
     dispatch(reserveDragon(id));
   };
+
+  const cancelReservationHandler = (id) => {
+    dispatch(cancelDragonReservation(id));
+  };
+
   return (
     <div className="dragon-component">
       <ul>
         {dragons.map((dragon) => (
           <li key={dragon.id}>
             {dragon.flickr_images.length > 0 && (
-              <img className="dragon-image" src={dragon.flickr_images} alt={`${dragon.name} Dragon`} />
+              <img
+                className="dragon-image"
+                src={dragon.flickr_images}
+                alt={`${dragon.name} Dragon`}
+              />
             )}
             <div className="desc">
               <h3 className="dragon-name">{dragon.name}</h3>
+              {dragon.reserved && <span className="reserved-badge">Reserved</span>}
               <p className="dragon-desc">{dragon.desc}</p>
               <p>
-                <button
-                  type="button"
-                  className="dragon-btn"
-                  onClick={() => {
-                    if (!dragon.reserved) {
-                      reserveHandler(dragon.id);
-                    }
-                  }}
-                >
-                  {dragon.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
-                </button>
+                {dragon.reserved ? (
+                  <button
+                    type="button"
+                    className="cancel-reservation  dragon-btn"
+                    onClick={() => cancelReservationHandler(dragon.id)}
+                  >
+                    Cancel Reservation
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="reserve-rocket-btn dragon-btn"
+                    onClick={() => reserveHandler(dragon.id)}
+                  >
+                    Reserve Dragon
+                  </button>
+                )}
               </p>
             </div>
           </li>
