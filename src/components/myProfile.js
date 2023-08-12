fed253682adf76636ef5f9814a247845d0b9118f
Sonika Kumar <sonikak004@gmail.com>
2023-08-12 08:59:35 +0100

    Add classes in myprofile.js to add css

--- src/components/myProfile.js
+++ src/components/myProfile.js
@@ -12,20 +12,30 @@ const Profile = () => {
   const joinedMissions = missions.filter((mission) => mission.reserved);
 
   return (
-    <div className="">
-      <div className="prof-rock-div">
-        <h2>My Rockets:</h2>
-        <ul className="prof-rock-ul">
+    <div className="prof-main-div">
+      <div className="profile-container">
+        <h2 className="mission-title">My Rockets</h2>
+        <ul className="mission-table">
           {reservedRockets.map((rocket) => (
-            <li className="prof-rockets" key={rocket.id}>{rocket.name}</li>
+            <li className="prof-rockets mission-row" key={rocket.id}>{rocket.name}</li>
           ))}
         </ul>
       </div>
-      <div className="prof-rock-div">
-        <h2>My Dragon:</h2>
-        <ul className="prof-rock-ul">
+      <div className="profile-container">
+        <h2 className="mission-title">My Dragon</h2>
+        <ul className="mission-table">
           {reservedDragons.map((dragon) => (
-            <li className="prof-rockets" key={dragon.id}>{dragon.name}</li>
+            <li className="prof-rockets mission-row" key={dragon.id}>{dragon.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="profile-container">
+        <h2 className="mission-title">My Missions</h2>
+        <ul className="mission-table">
+          {joinedMissions.map((mission) => (
+            <li key={mission.mission_id} className="mission-row">
+              {mission.mission_name}
+            </li>
           ))}
         </ul>
       </div>
